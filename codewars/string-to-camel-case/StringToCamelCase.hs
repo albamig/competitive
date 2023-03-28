247582d91793eac4b3d558c4150b945d14864549
Alberto <albamigh@gmail.com>
2023-03-28 20:53:32 +0200

    Solved string to camel case recursively

---
+++ codewars/string-to-camel-case/StringToCamelCase.hs
@@ -0,0 +1,11 @@
+module StringTocamelCase where
+
+import Data.Char
+
+toCamelCase :: String -> String
+toCamelCase [] = []
+toCamelCase (x:xs)
+    | x == '_' = capitalize xs
+    | x == '-' = capitalize xs
+    | otherwise = x : toCamelCase xs
+        where capitalize word = (toUpper $ head word) : (toCamelCase $ tail word)
