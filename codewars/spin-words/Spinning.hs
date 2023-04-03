b7ab36b46965629cbfa1fb22ea0e865e879a26d8
Alberto <albamigh@gmail.com>
2023-04-03 20:44:10 +0200

    Refactored spinning

--- codewars/spin-words/Spinning.hs
+++ codewars/spin-words/Spinning.hs
@@ -1,14 +1,10 @@
 module Spinning where
 
-import qualified Data.Text as T
-import qualified Data.List as L
-
 spinWords :: String -> String
-spinWords = L.intercalate " " . spin' . split'
+spinWords = unwords . spin . words
 
-split' :: String -> [String]
-split' str = map T.unpack (T.splitOn (T.pack " ") (T.pack str))
-
-spin' :: [String] -> [String]
-spin' = map cond_spin
-    where cond_spin str = if L.length  str >= 5 then L.reverse str else str
+spin :: [String] -> [String]
+spin = map cond_spin
+    where cond_spin str
+            | length str >= 5 = reverse str
+            | otherwise = str
