e20868701f6ced78237ffe69e1b43e283f14a224
Alberto <albamigh@gmail.com>
2023-04-06 14:23:39 +0200

    Scramblies performance improved

--- codewars/scramblies/Scramblies.hs
+++ codewars/scramblies/Scramblies.hs
@@ -3,7 +3,10 @@ module Scramblies where
 import Data.List
 
 scramble :: String -> String -> Bool
-scramble _ [] = True
-scramble s1 (s2_h:s2_t) 
+scramble s1 s2 = isSubsequenceOf (sort s2) (sort s1)
+
+scrambleSlow :: String -> String -> Bool
+scrambleSlow _ [] = True
+scrambleSlow s1 (s2_h:s2_t) 
     | s2_h `elem` s1 = scramble (delete s2_h s1) s2_t 
     | otherwise = False
